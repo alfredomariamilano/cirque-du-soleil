b8f795661711403dabb762734e7ab981c5e4ebc0
alfredomariamilano <alfmarmil@gmail.com>
2019-10-23 19:40:03 +1100

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -1,79 +1,86 @@
-$(document).ready(function($){
-  //cache some jQuery objects
-  var modalTrigger = $('.cd-modal-trigger'),
-    transitionLayer = $('.cd-transition-layer'),
-    transitionBackground = transitionLayer.children(),
-    modalWindow = $('.cd-modal');
+$(document).ready(($) => {
+  // cache some jQuery objects
+  const modalTrigger = $('.cd-modal-trigger');
+  const transitionLayer = $('.cd-transition-layer');
+  const transitionBackground = transitionLayer.children();
+  const modalWindow = $('.cd-modal');
 
-  var frameProportion = 1.78, //png frame aspect ratio
-    frames = 25, //number of png frames
-    resize = false;
+  const frameProportion = 1.78; // png frame aspect ratio
+  const frames = 25; // number of png frames
+  let resize = false;
 
-  //set transitionBackground dimentions
-  setLayerDimensions();
-  $(window).on('resize', function(){
-    if( !resize ) {
-      resize = true;
-      (!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
-    }
-  });
+  let textSize;
+  const originalText = $('.homeh1').text();
 
-  var textSize;
-
-  //open modal window
-  modalTrigger.on('click', function(event){
+  // open modal window
+  modalTrigger.on('click', (event) => {
     event.preventDefault();
     transitionLayer.addClass('visible opening');
-    var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 600;
-    setTimeout(function(){
+    const delay = ($('.no-cssanimations').length > 0) ? 0 : 600;
+    setTimeout(() => {
       textSize = $('.homeh1').css('font-size');
-      $('.homeh1').html('TE<br>QUIERO!<br>TI AMO!<br>I LOVE<br>YOU!');
+      $('.homeh1').html('TI<br>VOGLIAMO<br>BENE!');
       $('.homeh1').css('font-size', '8rem');
       $('#button').hide();
       modalWindow.addClass('visible');
     }, delay);
   });
 
-  //close modal window
-  modalWindow.on('click', '.modal-close', function(event){
+  // close modal window
+  modalWindow.on('click', '.modal-close', (event) => {
     event.preventDefault();
 
     $('.homeh1').animate({
-      fontSize: textSize
-    }, 5000, function() {
-      $('.homeh1').fadeOut(function() {
-        $(this).text('Buongiorno principessa!');
-      }).fadeIn(function() {
+      fontSize: textSize,
+    }, 5000, () => {
+      $('.homeh1').fadeOut(function () {
+        $(this).text(originalText);
+      }).fadeIn(() => {
         $('#button').fadeIn(1000);
       });
     });
 
     transitionLayer.addClass('closing');
     modalWindow.removeClass('visible');
-    transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+    transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', () => {
       transitionLayer.removeClass('closing opening visible');
       transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
     });
   });
 
   function setLayerDimensions() {
-    var windowWidth = $(window).width(),
-      windowHeight = $(window).height(),
-      layerHeight, layerWidth;
+    const windowWidth = $(window).width();
+    const windowHeight = $(window).height();
+    let layerHeight; let
+      layerWidth;
 
-    if( windowWidth/windowHeight > frameProportion ) {
+    if (windowWidth / windowHeight > frameProportion) {
       layerWidth = windowWidth;
-      layerHeight = layerWidth/frameProportion;
+      layerHeight = layerWidth / frameProportion;
     } else {
-      layerHeight = windowHeight*1.2;
-      layerWidth = layerHeight*frameProportion;
+      layerHeight = windowHeight * 1.2;
+      layerWidth = layerHeight * frameProportion;
     }
 
     transitionBackground.css({
-      'width': layerWidth*frames+'px',
-      'height': layerHeight+'px'
+      width: `${layerWidth * frames}px`,
+      height: `${layerHeight}px`,
     });
 
     resize = false;
   }
+
+  // set transitionBackground dimentions
+  setLayerDimensions();
+  $(window).on('resize', () => {
+    if (!resize) {
+      resize = true;
+
+      if (window.requestAnimationFrame) {
+        window.requestAnimationFrame(setLayerDimensions);
+      } else {
+        setTimeout(setLayerDimensions, 300);
+      }
+    }
+  });
 });
